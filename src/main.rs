0d112722b3ecba6e8936cdfacdd2edbc904afaa2
Zac Stewart <zgstewart@gmail.com>
2016-08-18 14:22:56 -0400

    Accept port as CLI argument

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ extern crate stun;
 use stun::{Client, Message};
 
 fn main() {
-    let client = Client::new("stun.l.google.com:19302", 6667);
+    let args: Vec<String> = std::env::args().collect();
+    let local_port = args[1].clone().parse::<u16>().unwrap();
+    let client = Client::new("stun.l.google.com:19302", local_port);
     let mesage = Message::request();
     let encoded = mesage.encode();
     let response = client.send(encoded.clone());
